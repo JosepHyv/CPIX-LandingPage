306850155752adc31d623f8d76d50e2622361146
Joseph Hynimoto <josephynimoto@gmail.com>
2024-11-19 11:39:19 -0600

    starting project adding fonts and style configs

---
+++ src/constants/colors.ts
@@ -0,0 +1,15 @@
+type ColorsPalete = {
+  red: string;
+  green: string;
+  yellow: string;
+  blue: string;
+};
+
+const Colors: ColorsPalete = {
+  red: "#e31521",
+  green: "#137d35",
+  yellow: "#f1ab32",
+  blue: "0991d0",
+};
+
+export { Colors };
